8e7c85507fcbf145a15116db4ed87af3ebc712d1
guest271314 <guest271314@gmail.com>
2024-06-24 21:49:36 -0700

    Include if condition to read length, multiple reads can occur initially and extension reload

--- nm_d8.js
+++ nm_d8.js
@@ -12,8 +12,43 @@ function encodeMessage(str) {
       // V8 authors are not interested in reading STDIN to an ArrayBuffer in d8
       // https://groups.google.com/g/v8-users/c/NsnStT6bx3Y/m/Yr_Z1FwgAQAJ
       // Use dd to get message length and return message from Bash script
+      // getNativeMessage.sh
+      // #!/bin/bash
+      // # Bash Native Messaging host
+      // # Read STDIN for V8's d8 shell, return message to d8
+      // # guest271314 2024
+      // set -x
+      // set -o posix
+      //
+      // getNativeMessage() {
+      //  # https://lists.gnu.org/archive/html/help-bash/2023-06/msg00036.html
+      //  length=$(dd iflag=fullblock oflag=nocache conv=notrunc,fdatasync bs=4 count=1 if=/proc/$@/fd/0 | od -An -td4 -)
+      //  message=$(dd iflag=fullblock oflag=nocache conv=notrunc,fdatasync bs=$((length)) count=1 if=/proc/$@/fd/0)
+      //  # GNU Coreutils head https://www.gnu.org/software/coreutils/manual/html_node/head-invocation.html
+      //  # length=$(head -q -z --bytes=4 /proc/$@/fd/0 | od -An -td4 -)
+      //  # message=$(head -q -z --bytes=$((length)) /proc/$@/fd/0)
+      //  echo "$message" 
+      // }
+      //
+      // getNativeMessage "$1"
       const stdin = (os.system("./getNativeMessage.sh", [pid])).trim();
-      return encodeMessage(stdin);
+      if (stdin != undefined && stdin != null && stdin.length) {
+        const message = encodeMessage(stdin.trim());
+        // https://stackoverflow.com/a/58288413
+        // const header = new Uint32Array([
+        //  ((uint32) =>
+        //    (uint32[3] << 24) |
+        //    (uint32[2] << 16) |
+        //    (uint32[1] << 8) |
+        //    (uint32[0]))(Array.from({
+        //    length: 4,
+        //    }, (_, index) => (message.length >> (index * 8)) & 0xff)),
+        // ]);
+        // writeFile("messageLength.txt", encodeMessage(JSON.stringify(header)));
+        return encodeMessage(stdin);
+     } else {
+       return;
+     }
     } catch (e) {
       writeFile("getMessageError.txt", encodeMessage(e.message));
     }

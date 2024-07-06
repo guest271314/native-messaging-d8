da363cc16e2f803a2e7aff74c48e0cd49d4c9b6e
guest271314 <guest271314@gmail.com>
2024-07-05 17:02:19 -0700

    Send message length to d8 from qjs

--- nm_d8.js
+++ nm_d8.js
@@ -12,58 +12,15 @@ function encodeMessage(str) {
       // V8 authors are not interested in reading STDIN to an ArrayBuffer in d8
       // https://groups.google.com/g/v8-users/c/NsnStT6bx3Y/m/Yr_Z1FwgAQAJ
       // Use QuickJS to get message length and return message from Bash script
-      // #!/usr/bin/env -S /home/user/bin/qjs -m --std
       // Read stdin to V8's d8, send to d8
-      // const stdin = os.system("./read_d8_stdin.js", [`/proc/${pid.replace(/\D+/g, "")}/fd/0`]);
-      // function read_d8_stdin([, path] = scriptArgs) {
-      //  try {
-      //    const size = new Uint32Array(1);
-      //    const err = { errno: 0 };
-      //    const pipe = std.open(
-      //      path,
-      //      "rb",
-      //      err,
-      //    );
-      //    if (err.errno !== 0) {
-      //      throw `${std.strerror(err.errno)}: ${path}`;
-      //    }
-      //    pipe.read(size.buffer, 0, 4);
-      //    const output = new Uint8Array(size[0]);
-      //    pipe.read(output.buffer, 0, output.length);
-      //    std.out.write(output.buffer, 0, output.length);
-      //    std.out.flush();
-      //    std.exit(0);
-      // } catch (e) {
-      //     const err = { errno: 0 };
-      //     const file = std.open("qjsErr.txt", "w", err);
-      //     if (err.errno !== 0) {
-      //       file.puts(JSON.stringify(err));
-      //       file.close();
-      //       std.exit(1);
-      //     }
-      //     file.puts(JSON.stringify(e));
-      //     file.close();
-      //     std.out.puts(JSON.stringify(err));
-      //     std.exit(1);
-      //   }
-      // }
-      //
-      // read_d8_stdin();
       const stdin = os.system("./read_d8_stdin.js", [`/proc/${pid.replace(/\D+/g, "")}/fd/0`]);
       if (stdin != undefined && stdin != null && stdin.length) {
-        const message = encodeMessage(stdin.trim());
-        // https://stackoverflow.com/a/58288413
-        // const header = new Uint32Array([
-        //  ((uint32) =>
-        //    (uint32[3] << 24) |
-        //    (uint32[2] << 16) |
-        //    (uint32[1] << 8) |
-        //    (uint32[0]))(Array.from({
-        //    length: 4,
-        //    }, (_, index) => (message.length >> (index * 8)) & 0xff)),
-        // ]);
-        // writeFile("messageLength.txt", encodeMessage(JSON.stringify(header)));
-        return encodeMessage(stdin);
+        const data = encodeMessage(stdin)
+        const view = new DataView(data.subarray(0, 4).buffer);
+        const length = view.getUint32(0, true);
+        sendMessage(encodeMessage(JSON.stringify({ length })));
+        const message = data.subarray(4);
+        return message;
      } 
      throw stdin;
     } catch (e) {
--- read_d8_stdin.js
+++ read_d8_stdin.js
@@ -1,37 +1,42 @@
 #!/usr/bin/env -S /home/user/bin/qjs -m --std
-// Read stdin to V8's d8, send to d8
+ // Read stdin to V8's d8, send to d8
 // const stdin = os.system("./read_d8_stdin.js", [`/proc/${pid.replace(/\D+/g, "")}/fd/0`]);
 function read_d8_stdin([, path] = scriptArgs) {
-    try {
-      const size = new Uint32Array(1);
-      const err = { errno: 0 };
-      const pipe = std.open(
-        path,
-        "rb",
-        err,
-      );
-      if (err.errno !== 0) {
-        throw `${std.strerror(err.errno)}: ${path}`;
-      }
-      pipe.read(size.buffer, 0, 4);
-      const output = new Uint8Array(size[0]);
-      pipe.read(output.buffer, 0, output.length);
-      std.out.write(output.buffer, 0, output.length);
-      std.out.flush();
-      std.exit(0);
-    } catch (e) {
-      const err = { errno: 0 };
-      const file = std.open("qjsErr.txt", "w", err);
-      if (err.errno !== 0) {
-        file.puts(JSON.stringify(err));
-        file.close();
-        std.exit(1);
-      }
-      file.puts(JSON.stringify(e));
+  try {
+    const size = new Uint32Array(1);
+    const err = {
+      errno: 0
+    };
+    const pipe = std.open(
+      path,
+      "rb",
+      err,
+    );
+    if (err.errno !== 0) {
+      throw `${std.strerror(err.errno)}: ${path}`;
+    }
+    pipe.read(size.buffer, 0, 4);
+    const output = new Uint8Array(size[0]);
+    pipe.read(output.buffer, 0, output.length);
+    const data = new Uint8Array([...new Uint8Array(size.buffer), ...output]);
+    std.out.write(data.buffer, 0, data.length);
+    std.out.flush();
+    std.exit(0);
+  } catch (e) {
+    const err = {
+      errno: 0
+    };
+    const file = std.open("qjsErr.txt", "w", err);
+    if (err.errno !== 0) {
+      file.puts(JSON.stringify(err));
       file.close();
-      std.out.puts(JSON.stringify(err));
       std.exit(1);
     }
+    file.puts(JSON.stringify(e));
+    file.close();
+    std.out.puts(JSON.stringify(err));
+    std.exit(1);
   }
-  
-  read_d8_stdin();
+}
+
+read_d8_stdin();

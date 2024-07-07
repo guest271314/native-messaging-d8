7b87259e12a2fe1c344190a67202061a0dffe201
guest271314 <guest271314@gmail.com>
2024-07-07 09:06:36 -0700

    Update read_d8_stdin.js

--- read_d8_stdin.js
+++ read_d8_stdin.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env -S /home/user/bin/qjs -m --std
- // Read stdin to V8's d8, send to d8
+// Read stdin to V8's d8, send to d8
 // const stdin = os.system("./read_d8_stdin.js", [`/proc/${pid.replace(/\D+/g, "")}/fd/0`]);
 function read_d8_stdin([, path] = scriptArgs) {
   try {
+    // Uint32Array to read message length into
     const size = new Uint32Array(1);
     const err = {
       errno: 0
     };
+    // Open /proc/PID/fd/0 for reading
     const pipe = std.open(
       path,
       "rb",
@@ -15,14 +17,21 @@ function read_d8_stdin([, path] = scriptArgs) {
     if (err.errno !== 0) {
       throw `${std.strerror(err.errno)}: ${path}`;
     }
+    // Read /proc/PID/fd/0 into Uint32Array
     pipe.read(size.buffer, 0, 4);
+    // Uint8Array to read message into
     const output = new Uint8Array(size[0]);
+    // Read /proc/PID/fd/0 into Uint8Array
     pipe.read(output.buffer, 0, output.length);
+    // Uint8Array containing Uint32Array buffer in Uint8Array and Uint8Array containing message
     const data = new Uint8Array([...new Uint8Array(size.buffer), ...output]);
+    // Write to d8's stdin
     std.out.write(data.buffer, 0, data.length);
     std.out.flush();
+    // Exit and start again initiated from d8
     std.exit(0);
   } catch (e) {
+    // Handle error
     const err = {
       errno: 0
     };
